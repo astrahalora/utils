b7f8e0a1c220c6331b921c1f71e3bb038523f2cf
astrahalora <astrahalora@gmail.com>
2024-05-13 19:13:36 +0300

    Add stringSearch

--- binarySearch.js
+++ binarySearch.js
@@ -2,15 +2,15 @@
 
 function search(arr, num) {
     let min = 0;
-    let max = arr.length -1;
+    let max = arr.length - 1;
 
-    while(min <= max) {
+    while (min <= max) {
         let middle = Math.floor((min + max) / 2);
         let currentElement = arr[middle];
 
-        if(currentElement < num) {
+        if (currentElement < num) {
             min = middle + 1;
-        } else if( currentElement > num) {
+        } else if (currentElement > num) {
             max = middle - 1;
         } else {
             return middle;
@@ -72,4 +72,3 @@ const americanStates = [
     "Wisconsin",
     "Wyoming"
 ];
-
