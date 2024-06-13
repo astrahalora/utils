de378113d42ea9ee1d71fce4d86c7a6c4be9c3ef
astrahalora <astrahalora@gmail.com>
2024-06-13 17:27:51 +0300

    Add basics of classes

---
+++ class.js
@@ -0,0 +1,14 @@
+class SomeClass {
+    constructor() {
+        //What default properties should it have?
+    }
+
+    someInstanceMethod() {
+        //What should each object created from this class be able to do?
+    }
+
+    static someClassMethod() {
+        //What should this class be able to do?
+        //SomeClass.someClassMethod()
+    }
+}
