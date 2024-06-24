94f7ed3558e85cbcc51867b6d97c7fa013be24e1
astrahalora <astrahalora@gmail.com>
2024-06-24 21:26:14 +0300

    Add shift and unshift to doubly

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -31,8 +31,55 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    pop() {
+        if(!this.head) return undefined;
+        const oldTail = this.tail;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = oldTail.prev;
+            this.tail.next = null;
+            oldTail.prev = null;
+        }
+        this.length--;
+        return oldTail;
+    }
+
+    shift() {
+        if(this.length === 0) return undefined;
+        const oldHead = this.head;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead
+    }
+
+    unshift(val) {
+        const newNode = new Node(val);
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+     }
 }
 
 let firstDoublyList = new DoublyLinkedList();
 firstDoublyList.push("99");
-display(firstDoublyList.push("22"));
+firstDoublyList.push("22");
+firstDoublyList.push("11");
+display(firstDoublyList.pop());
+display(firstDoublyList)
