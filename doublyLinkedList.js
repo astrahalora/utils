5a2fd43366ae948341baa44660e02ec48f6badd6
astrahalora <astrahalora@gmail.com>
2024-06-24 19:38:47 +0300

    Add doublyLinkedList base with push

---
+++ doublyLinkedList.js
@@ -0,0 +1,38 @@
+// reference to the previous and next Node
+const display = (content) => {
+    console.log(content);
+}
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let firstDoublyList = new DoublyLinkedList();
+firstDoublyList.push("99");
+display(firstDoublyList.push("22"));
